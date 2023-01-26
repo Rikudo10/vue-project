d83ef5cbd176443d3c37e1e5a4e5ddc903820ef2
Rikudo10 <giorgisamxaradze16@gmail.com>
2023-01-26 18:04:59 +0400

    pirveli

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,55 @@
 <script>
-import Listmenu from "./List.vue"
 
-export default{
-  components: {
-    Listmenu
-  },
-  data(){
+export default {
+  data() {
     return {
-      dataItem: [
-                {
-                  id: 1,
-                  name:"HOME",
-                  url:"https://www.google.com/",
-                  description: "Home-Pages"
-                },
-                {
-                  id: 2,
-                  name:"CONTACT",
-                  url:"https://www.bing.com/?toWww=1&redig=8B85287D87334880A26C4E35E8CE5234",
-                  description: "CONTACT-PAGE"
-                },
-                {
-                  id: 3,
-                  name:"ABOUT US",
-                  url:"https://yandex.com/",
-                  description: "ABOUT-PAGE"
-                },
-                {
-                  id: 4,
-                  name:"INFO",
-                  url:"https://duckduckgo.com/",
-                  description: "INFO-PAGE"
-                },
-              ]
+      width: 20,
+      height: 30,
+      simagle: null,
+      show: false
+    }
+  },
+  computed: {
+    calculateSpace(){
+      return this.width * this.height
+    },
+    calculateMoculoba(){
+      return this.calculateSpace * this.simagle
     }
   }
 }
+
 </script>
 
 <template>
-  <Listmenu :items="dataItem"></Listmenu>
+  <div>
+    <form>
+
+      <label>სიგრძე</label><br />
+      <input v-model="width" /><br />
+
+      <label>სიგანე</label><br />
+      <input v-model="height" /><br />
+      
+      <label>სიმაღლე</label><br />
+      <input v-model="simagle" /><br />
+    </form>
+    <br />
+    <button @click.prevent="show = !show">
+      <span v-if="!show">Show</span>
+      <span v-if="show">Hide</span>
+    </button>
+    <p v-if="show">ფართობი: {{ calculateSpace }}</p>
+    <p v-if="show">მოცულობა: {{ calculateMoculoba }}</p>
+  </div>
+
+  <div v-bind:style="{ width: width + 'px',height: height + 'px' },{height: height + 'px'}" class="div1">
+  
+  </div>
 </template>
+
+<style>
+ .div1 {
+  border: 3px solid green;
+ }
+</style>
