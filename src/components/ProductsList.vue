061cc3a1c4316b85e2a5d464b0214d4c15befebc
Rikudo10 <giorgisamxaradze16@gmail.com>
2023-02-07 21:06:31 +0400

    two commit

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -2,6 +2,13 @@
 import { useStore } from 'vuex'
 import { computed } from 'vue'
 
+defineProps({
+    data: {
+        type: String,
+        required: true
+    }
+})
+
 const store = useStore()
 const data = computed(() => store.getters.productsList)
 
@@ -11,7 +18,7 @@ const data = computed(() => store.getters.productsList)
         <h2 class="text-2xl font-bold">ყველა  ვიდეო </h2>
         <ul class="w-60 space-y-2">
             <li v-for="item in data" :key="item.index" class="flex flex-row justify-between items-center">
-                <span>{{ item.name }} - {{ item.price }}</span>
+                <span>{{ item.name }} - <img src="item.cover"></span>
                 <button 
                     class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
                     @click.prevent="store.dispatch('addItemToCartAction', item.id)"
--- store/state.js
+++ store/state.js
@@ -1,7 +1,10 @@
 export default function state() {
     return {
         products: [
-            { id: 1, name: 'product 1', price: 100 },
+            { id: 1, name: 'GNB Sukhishvili - Juta',
+              url: 'https://www.youtube.com/watch?v=9sBCkTC-Y40&list=RD9sBCkTC-Y40&start_radio=1',
+              description: 'სუხიშვილები',
+              cover: 'https://madloba.info/media/images/Concert-of-Georgian-National-Ballet-Sukhishvil.width-800.jpg'  },
             { id: 2, name: 'product 2', price: 200 },
             { id: 3, name: 'product 3', price: 300 },
             { id: 4, name: 'product 4', price: 400 },
