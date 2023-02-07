789b94206da0a5b061052b2422e3b1178c8d1a92
Rikudo10 <giorgisamxaradze16@gmail.com>
2023-02-07 23:04:52 +0400

    project

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,16 @@ import ProductsList from './components/ProductsList.vue'
 import Cart from './components/Cart.vue'
 import Product from './components/Product.vue'
 
+const store = useStore()
 
 </script>
 
 <template>
   <div class="container mx-auto text-center">
+    <input type="text" v-model="search" class="border-4 border-indigo-500/100">
+    <div v-for="product in filteredPost" :key="product.id">
+      {{ product.name }}
+    </div>
     <div class="flex flex-row space-x-4">
       <ProductsList />
       <Cart />
