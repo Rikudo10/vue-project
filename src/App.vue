71fb5d3ae0010a8ad61492814cd7d6ca6cdbca84
Rikudo10 <giorgisamxaradze16@gmail.com>
2023-02-08 18:14:06 +0400

    router

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,21 @@ import watched from './components/watched.vue'
 import video from './components/video.vue'
 
 
+const menu = [
+  { routeName: 'MTAVARI', title: 'Main Page'},
+  { routeName: 'About', title: 'About Us'},
+  { routeName: 'Contact', title: 'Contac Us'}
+]
+
 </script>
 
 <template>
+   <div class="flex flex-row space-x-4">
+      <RouterLink v-for="item in menu" :key="item.index" :to="{ name: item.routeName }">{{ item.title }}</RouterLink>
+    </div>
+    <div>
+      <RouterView />
+    </div>
   <div class="container mx-auto text-center">
     
     <div class="flex flex-row space-x-4">
--- src/main.js
+++ src/main.js
@@ -4,6 +4,35 @@ import store from '../store'
 
 import './assets/main.css'
 
-  createApp(App)
-  .use(store)
-  .mount('#app')
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Contact from './views/Contact.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: "MTAVARI",
+        component: Home
+    },
+    {
+        path: '/about',
+        name: "About",
+        component: About
+    },
+    {
+        path: '/contact',
+        name: "Contact",
+        component: Contact
+    },
+]
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: routes
+})
+
+createApp(App)
+.use(store)
+.use(router)
+.mount('#app')
